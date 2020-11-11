beab96bdeeb69ea6de5e1670bddedfeacb63f528
sai manikanta <70973798+saimani008@users.noreply.github.com>
2020-11-11 22:44:21 +0530

    Update unique.c

--- clab/unique.c
+++ clab/unique.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int arr[10],freq[100],n,count = 1;
+    int arr[100],freq[100],n,count;
     printf("enter the no of elements :"); 
     scanf("%d",&n);
     printf("enter the  elements in array \n"); 
@@ -11,16 +11,18 @@ int main()
     }
     for(int i=0;i<n;i++)
     {
-       for(int j=j+1;j<n;j++)
+        count =1;
+       for(int j=i+1;j<n;j++)
        {
           if(arr[i]==arr[j])
           {
              count++;
+              freq[j] =0;
           }
-          else
-          {
-             freq[i] = count;
-          }
+       }
+       if(freq[i]!=0)
+       {
+           freq[i] = count;
        }
      }
      printf("the unique elements are : \n");
